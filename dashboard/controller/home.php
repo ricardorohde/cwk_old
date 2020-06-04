95f1e8ae1105f0003fcb8b2bad28e3c7e09867cb
Gabriel Carvalho Gama <gabriel.carvalhogama@hotmail.com>
2020-06-04 00:39:29 -0300

    Updated rules

--- dashboard/controller/home.php
+++ dashboard/controller/home.php
@@ -1,29 +1,23 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-10">
-    <blockquote>Bem vindo (a) ao <strong>SISTEMA DE AGENDAMENTO - COWORKING LINHARES</strong>, utilize o menu lateral para navegar entre as opções<br></blockquote>
+    <blockquote>Bem-vindo (a) ao SISTEMA DE AGENDAMENTO - COWORKING LINHARES, utilize o menu lateral para navegar entre as opções.</blockquote>
 <strong>MANUAL DO SISTEMA:</strong>
 <ul>
- 	<li>No menu lateral aparecerá todos os serviços contratados pelo cliente. Dentro de cada serviço o cliente poderá abrir a agenda e visualizar os agendamentos assim como realizar agendamentos. Poderá visualizar o histórico de agendamentos realizados e excluir agendamentos que por ventura foram cancelados ou remarcados.</li>
+ 	<li>No menu lateral aparecerá todos os serviços contratados pelo cliente. Dentro de cada serviço, o cliente poderá abrir a agenda e visualizar os agendamentos assim como realizar agendamentos. Poderá visualizar o histórico de agendamentos realizados e excluir agendamentos que por ventura forem cancelados ou remarcados.</li>
 </ul>
 <strong>CADASTRANDO NOVO AGENDAMENTO</strong>
 <ul>
- 	<li>1 PASSO</li>
- 	<li>No menu lateral clicar em Agenda do serviço que deseja realizar o agendamento.</li>
- 	<li>2 PASSO</li>
- 	<li>Basta clicar,segurar e arrastar no dia e hora desejado no calendario que aparecer.</li>
-	<li>3 PASSO</li>
- 	<li>Preencher informações e concluir agendamento.</li>
+ 	<li>1º Passo: No menu lateral clicar em Agenda do serviço que deseja realizar o agendamento.</li>
+ 	<li>2º Passo: Basta clicar, segurar e arrastar no dia e hora desejado no calendário que aparecer.</li>
+ 	<li>3º Passo: Preencher informações, colocando nome completo + empresa/Ramo de atividade e concluir agendamento.</li>
 </ul>
 
 <strong>EXCLUIR AGENDAMENTO</strong>
 <ul>
- 	<li>1 PASSO</li>
- 	<li>No menu lateral clicar em Histórico do serviço que deseja realizar a exclusão do agendamento.</li>
- 	<li>2 PASSO</li>
- 	<li>Na lista dos agendamentos identifique o agendamento que deseja excluir</li>
-	<li>3 PASSO</li>
- 	<li>Na coluna açoes clique no botão excluir referente a linha do agendamento desejado.</li>
+ 	<li>1º Passo: No menu lateral clicar em Histórico do serviço que deseja realizar a exclusão do agendamento.</li>
+ 	<li>2º Passo: Na lista dos agendamentos identifique o agendamento que deseja excluir</li>
+ 	<li>3º Passo: Na coluna ações clique no botão excluir referente a linha do agendamento desejado.</li>
 </ul>
 
 <strong>HISTÓRICO DE AGENDAMENTOS</strong>
@@ -32,12 +26,7 @@
 </ul>
 <strong>REGRAS DE AGENDAMENTOS</strong>
 <ul>
- 	<li>Os agendamento pode ser realizado a qualquer momento, o sistema estará disponivel 24h/dia.</li>
-	<li>Não é permido 2 agendamentos no mesmo horário. Nesse caso quem registrou primeiro terá a preferência.</li>
-	<li>O agendamento estará disponível para exclusão até 3h de antecedência ao horário agendado.</li>
-	<li>Caso queira realizar o cancelamento ou remarcação dentro dessas 3h que antecedem o horario do agendamento. Será cobrado uma taxa equivalente a meia hora (1/2h) desse serviço ou descontado dos créditos de pacotes de horas. </li>
-	<li>No caso de não comparecimento ou não cancelamento do agendamento, será cobrado o valor da hora integral do serviço ou descontado dos créditos de pacotes de horas.</li>
-	<li>É de responsabilidade do cliente conferir na agenda do serviço se apareceu seu agendamento corretamente.</li>
+ 	<li>Para acessar o Histórico de agendamentos vá até o menu lateral, clique em Histórico do serviço que deseja visualizar os agendamentos.</li>
 </ul>
         </div>
     </div>
